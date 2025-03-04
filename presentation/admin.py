f951752a82bf630983f8cb4760a941ef0a3e9be5
NMelis <melis.zhoroev@gmail.com>
2025-03-04 20:43:19 +0600

    admin panel search filter and e.t.c

--- presentation/admin.py
+++ presentation/admin.py
@@ -9,26 +9,35 @@ admin.site.register([
 @admin.register(Presentation)
 class PresentationAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'created_at', 'updated_at', 'share_link_uid')
+    search_fields = ('id', 'user__username', 'user__email', 'title', 'share_link_uid')
+    list_filter = ('date_created', 'date_edited', 'group', 'removed', 'user')
+
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['user', 'amount', 'currency', 'status', 'order_id', 'created_at']
     search_fields = ['user__username', 'amount', 'order_id', 'created_at']
+    list_filter = ('status', 'created_at', 'user')
 
 
 @admin.register(Tariff)
 class TariffAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'presentation_count']
+    search_fields = ['name', 'price', 'presentation_count']
 
 
 @admin.register(Balance)
 class BalanceAdmin(admin.ModelAdmin):
-    list_display = ['amount']
+    list_display = ['user_balance', 'amount', 'next_payment_at']
+    list_filter = ['next_payment_at']
+    search_fields = ['user_balance__email', 'user_balance__username']
 
 
 @admin.register(BalanceHistory)
 class BalanceHistoryAdmin(admin.ModelAdmin):
-    list_display = ['amount_change', 'change_type', 'change_reason', 'balance']
+    list_display = ['amount_change', 'change_type', 'change_reason', 'balance', 'created_at']
+    search_fields = ['balance__user_balance__username', 'balance__user_balance__email']
+    list_filter = ['change_type', 'change_reason', 'created_at']
 
 
 @admin.register(PromoCode)
