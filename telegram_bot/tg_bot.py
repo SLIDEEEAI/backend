f2563201213259e4383e26d75486d46d2e9a730f
NMelis <melis.zhoroev@gmail.com>
2025-04-08 03:40:15 +0600

    f

--- telegram_bot/tg_bot.py
+++ telegram_bot/tg_bot.py
@@ -2,4 +2,7 @@ import telebot
 
 from main.models import Config
 
-bot = telebot.TeleBot(Config.get_instance().telegram_bot_apikey)
+try:
+    bot = telebot.TeleBot(Config.get_instance().telegram_bot_apikey)
+except:
+    bot = telebot.TeleBot('')
