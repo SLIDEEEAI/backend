c73d3ac42a2a7de9aadec339681e6890785fa469
BboyHookOff <kryuchkov.grisha@gmail.com>
2026-04-10 00:19:22 +0300

    Перенос настройки мода и секретного ключа джанго в .env

--- source/settings.py
+++ source/settings.py
@@ -32,10 +32,10 @@ BASE_URL = 'https://slideee.ru'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m-!$bp7@^cd7@5ng3!2(he(%rx!s@-2t4zel+xue6=3cnx+p(m'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if config("DEBUG_MODE") == "True" else False
 
 ALLOWED_HOSTS = ['*']
 
